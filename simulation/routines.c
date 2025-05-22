c87e645115a2af7fe029e3acacd3090e27a8b99c
Javiertries2code <superjabo@live.com>
2025-05-22 20:36:54 +0200

    proceeding to fit to norm

--- simulation/routines.c
+++ simulation/routines.c
@@ -1,110 +1,82 @@
 #include "../philo.h"
-int eating(t_philo *philo)
+
+int	eating(t_philo *philo)
 {
-    int var_status;
+	int	var_status;
 
-    safe_mutex(philo->first_fork, LOCK);
-    var_status = all_alive(philo, FORK);
-    if (var_status != ALL_ALIVE) {
-        safe_mutex(philo->first_fork, UNLOCK);
-        return var_status;
-    }
-    //TEST, this should do well enough in case of one only filo
-    if(philo->settings->num_philosophers == 1)
-    {
-        safe_mutex(philo->first_fork, UNLOCK);
-        return ONE_DIED;    
-    }
-
-    safe_mutex(philo->second_fork, LOCK);
-
-       //has dinner just when entering
-        safe_mutex(philo->meal_mtx, LOCK);
-        philo->last_meal = get_time(NULL, GET, MILISECONDS);
-        safe_mutex(philo->meal_mtx, UNLOCK);
-
-    var_status = all_alive(philo, FORK2);
-    if (var_status != ALL_ALIVE) {
-        safe_mutex(philo->second_fork, UNLOCK);
-        safe_mutex(philo->first_fork, UNLOCK);
-        return var_status;
-    }
-
- 
-
-    precise_sleep(philo->time_to_eat, &philo->threshold);
-    safe_mutex(philo->meal_mtx, LOCK);
-
-    philo->max_meals--;
-    safe_mutex(philo->meal_mtx, UNLOCK);
-
-    if (philo->max_meals == 0)
-    {
-        //TEST FULL
-        // safe_mutex(philo->t_write_mtx, LOCK);
-        // printf(YELLOW "%ld  [%ld] is FULL in eating\n" RESET,
-        //        (get_time(NULL, GET, MILISECONDS) - philo->settings->starting_time),
-        //        philo->philo_id);
-        // safe_mutex(philo->t_write_mtx, UNLOCK);
-
-        safe_mutex(philo->status_mtx, LOCK);
-        *philo->return_status= FULL;
-        safe_mutex(philo->status_mtx, UNLOCK);
-
-        safe_mutex(philo->first_fork, UNLOCK);
-        safe_mutex(philo->second_fork, UNLOCK);
-        return FULL;
-    }
-
-    safe_mutex(philo->first_fork, UNLOCK);
-    safe_mutex(philo->second_fork, UNLOCK);
-    return var_status;
+	safe_mutex(philo->first_fork, LOCK);
+	var_status = all_alive(philo, FORK);
+	if (var_status != ALL_ALIVE)
+	{
+		safe_mutex(philo->first_fork, UNLOCK);
+		return (var_status);
+	}
+	if (philo->settings->num_philosophers == 1)
+	{
+		safe_mutex(philo->first_fork, UNLOCK);
+		return (ONE_DIED);
+	}
+	safe_mutex(philo->second_fork, LOCK);
+	// has dinner just when entering
+	safe_mutex(philo->meal_mtx, LOCK);
+	philo->last_meal = get_time(NULL, GET, MILISECONDS);
+	safe_mutex(philo->meal_mtx, UNLOCK);
+	var_status = all_alive(philo, FORK2);
+	if (var_status != ALL_ALIVE)
+	{
+		safe_mutex(philo->second_fork, UNLOCK);
+		safe_mutex(philo->first_fork, UNLOCK);
+		return (var_status);
+	}
+	precise_sleep(philo->time_to_eat, &philo->threshold);
+	safe_mutex(philo->meal_mtx, LOCK);
+	philo->max_meals--;
+	safe_mutex(philo->meal_mtx, UNLOCK);
+	if (philo->max_meals == 0)
+	{
+		safe_mutex(philo->status_mtx, LOCK);
+		*philo->return_status = FULL;
+		safe_mutex(philo->status_mtx, UNLOCK);
+		safe_mutex(philo->first_fork, UNLOCK);
+		safe_mutex(philo->second_fork, UNLOCK);
+		return (FULL);
+	}
+	safe_mutex(philo->first_fork, UNLOCK);
+	safe_mutex(philo->second_fork, UNLOCK);
+	return (var_status);
 }
 
-
-long int time_left(t_philo *philo)
+long int	time_left(t_philo *philo)
 {
-    long int time_left;
-    long int last_meal;
-    //    safe_mutex(&philo->settings->status_mtx[philo->philo_id - 1], LOCK);
-    safe_mutex(philo->meal_mtx, LOCK);
-    last_meal = philo->last_meal;
-    safe_mutex(philo->meal_mtx, UNLOCK);
-    // safe_mutex(&philo->settings->status_mtx[philo->philo_id - 1], UNLOCK);
-    time_left = philo->time_to_die - (get_time(NULL, GET, MILISECONDS) - last_meal);
+	long int	time_left;
+	long int	last_meal;
 
-
-    return (time_left);
+	safe_mutex(philo->meal_mtx, LOCK);
+	last_meal = philo->last_meal;
+	safe_mutex(philo->meal_mtx, UNLOCK);
+	time_left = philo->time_to_die - (get_time(NULL, GET, MILISECONDS)
+			- last_meal);
+	return (time_left);
 }
 
-int thinking(t_philo *philo)
+int	thinking(t_philo *philo)
 {
+	int		var_status;
+	long	thinking_time;
 
-    int var_status;
-    long thinking_time;
-    thinking_time = philo->time_to_die - (philo->time_to_eat + philo->time_to_sleep);
-
-    var_status = all_alive(philo, THINKING);
-    //ACCESING philo->settings->num_philosophers would create a race, gotta load in struct
-    //ACCESING philo->settings->num_philosophers would create a race, gotta load in struct
-    //ACCESING philo->settings->num_philosophers would create a race, gotta load in struct
-    //ACCESING philo->settings->num_philosophers would create a race, gotta load in struct
-    if (philo->philo_id % 2 != 0 && philo->settings->num_philosophers % 2 != 0 )
-        precise_sleep(thinking_time/4, &philo->threshold);
-
-    return (var_status);
+	thinking_time = philo->time_to_die - (philo->time_to_eat
+			+ philo->time_to_sleep);
+	var_status = all_alive(philo, THINKING);
+	if (philo->philo_id % 2 != 0 && philo->settings->num_philosophers % 2 != 0)
+		precise_sleep(thinking_time / 4, &philo->threshold);
+	return (var_status);
 }
 
-int sleeping(t_philo *philo)
+int	sleeping(t_philo *philo)
 {
-    // support_read_returns(philo->settings);
+	int	var_status;
 
-    int var_status;
-
-  
-
-    var_status = all_alive(philo, SLEEPING);
-
-    precise_sleep(philo->time_to_sleep, &philo->threshold);
-    return var_status;
+	var_status = all_alive(philo, SLEEPING);
+	precise_sleep(philo->time_to_sleep, &philo->threshold);
+	return (var_status);
 }
