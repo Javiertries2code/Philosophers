11e672bc492bf90bb8c07ef599334a34f763cd00
Javiertries2code <superjabo@live.com>
2025-06-01 15:56:50 +0200

    handler ok, procceding to main

--- handlers/safe_mutex.c
+++ handlers/safe_mutex.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   safe_mutex.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: jbravo <jbravo@student.42.fr>              +#+  +:+       +#+        */
+/*   By: havr <havr@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/06/01 13:55:46 by jbravo            #+#    #+#             */
-/*   Updated: 2025/06/01 13:55:48 by jbravo           ###   ########.fr       */
+/*   Updated: 2025/06/01 15:56:28 by havr             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,9 @@ void	init_error(int error_return)
 	if (error_return == ENOMEM)
 		exit_on_error("low memory to initialize the mutex.");
 	if (error_return == EAGAIN)
-		exit_on_error("Lacked the necessary resources (other than memory) to initialize mutex.");
+		exit_on_error("Lacked the necessary resources (other than memory).");
 	if (error_return == EPERM)
-		exit_on_error("The caller does not have privilege to perform the operation.");
+		exit_on_error("Needs privilege to perform the operation.");
 }
 
 void	lock_error(int error_return)
@@ -31,13 +31,13 @@ void	lock_error(int error_return)
 	if (error_return == EDEADLK)
 		exit_on_error("Current thread already owns the mutex.");
 	if (error_return == EAGAIN)
-		exit_on_error("The mutex wasnt acquired because max number of recursive locks for mutex was exceeded.");
+		exit_on_error("Max number of recursive locks for mutex was exceeded.");
 	if (error_return == EOWNERDEAD)
-		exit_on_error("The last owner of this mutex terminated while holding the mutex.");
+		exit_on_error("The last owner terminated while holding the mutex.");
 	if (error_return == ENOTRECOVERABLE)
-		exit_on_error("The mutex you are trying to acquire was protecting state left irrecoverable by the mutexs previous owner.");
+		exit_on_error("Mutex left irrecoverable by the mutexs previous owner.");
 	if (error_return == ENOMEM)
-		exit_on_error("Limit on the number of simultaneously held mutexes has been exceeded.");
+		exit_on_error("Limit of simultaneously held mutexes exceeded.");
 }
 
 void	destroy_error(int error_return)
@@ -45,7 +45,7 @@ void	destroy_error(int error_return)
 	if (!error_return)
 		return ;
 	if (error_return == EBUSY)
-		exit_on_error("Attempt to destroy object referenced by mutex while it was locked.");
+		exit_on_error("Attempt to destroy object while locked.");
 	if (error_return == EINVAL)
 		exit_on_error("The value specified by mutex not valid.");
 }
